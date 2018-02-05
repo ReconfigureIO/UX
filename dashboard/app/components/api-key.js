b86dfbe8ec5cf2524454427b3e16eb726edbf649
Sean Jessen <sean@ayup.agency>
2018-02-05 11:26:40 +0000

    Added copy to clipboard button for API key

--- dashboard/app/components/api-key.js
+++ dashboard/app/components/api-key.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  init: function() {
+    this._super(...arguments);
+    new Clipboard('.btn');
+  },
   actions: {
     toggleKey: function() {
       if(Ember.$('.api-key__container__key').attr('type') == 'password') {
@@ -9,8 +13,9 @@ export default Ember.Component.extend({
         Ember.$('.api-key__container__key').attr('type', 'password');
       }
       Ember.$('.api-key__container__trigger').text(function(i, text){
-          return text === "Show API key" ? "Hide API key" : "Show API key";
+        return text === "Show API key" ? "Hide API key" : "Show API key";
       });
+      Ember.$('.api-key__container__copy').toggleClass('active');
     }
   }
 });
