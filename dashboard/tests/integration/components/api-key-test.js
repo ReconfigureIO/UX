5e45804585f3abf06e8073bed420932f6fd9fcae
Max Siegieda <maxsiegieda@gmail.com>
2018-03-07 11:53:53 +0000

    fix or remove failing tests

--- dashboard/tests/integration/components/api-key-test.js
+++ dashboard/tests/integration/components/api-key-test.js
@@ -11,14 +11,6 @@ test('it renders', function(assert) {
 
   this.render(hbs`{{api-key}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.notEqual(this.$().text().trim(), '');
 
-  // Template block usage:
-  this.render(hbs`
-    {{#api-key}}
-      template block text
-    {{/api-key}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });
