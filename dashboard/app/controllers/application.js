9ffcbdd95b4f38afd5bc98e05ec6af4f0297ce77
Max Siegieda <maxsiegieda@gmail.com>
2018-03-29 13:45:24 +0100

    Hide sidebar on signup pages

--- dashboard/app/controllers/application.js
+++ dashboard/app/controllers/application.js
@@ -2,7 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   isIndex: Ember.computed('currentRouteName',function(){
-    if(this.get('currentRouteName') === 'index'){
+    if(this.get('currentRouteName') === 'index' || 'sign-up/installation'){
+      return true;
+    }
+    return false;
+  }),
+  hideSideBar: Ember.computed('currentRouteName',function(){
+    if(this.get('currentRouteName') === 'index' || 'sign-up/*'){
       return true;
     }
     return false;
@@ -15,7 +21,7 @@ export default Ember.Controller.extend({
      return false;
     }
   }.property('currentPath'),
-  showSideBar: function(){
+  showSubNav: function(){
     var curPath =this.get('currentPath');
     if(curPath == 'account.details' || curPath == 'account.billing' || curPath == 'account.api'){
       return true;
