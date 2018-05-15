4bb90f81e7d580f7dcbe48dde7c5505441e014e9
Rosie Yohannan <rosie.yohannan@reconfigure.io>
2018-05-15 11:05:36 +0100

    re-hide sidebar

--- dashboard/app/controllers/application.js
+++ dashboard/app/controllers/application.js
@@ -7,14 +7,14 @@ export default Ember.Controller.extend({
     }
     return false;
   }),
-  //hideSideBar: Ember.computed('currentRouteName',function(){
-  //  var curPath = this.get('currentRouteName');
-  //  console.log(curPath);
-  //  if(curPath === 'index' || curPath === 'sign-up.installation' || curPath === 'sign-up.auth' || curPath === 'sign-up.choose-plan' || curPath === 'sign-up.goal' || curPath === 'sign-up.getting-started'){
-  //    return true;
-  //  }
-  //  return false;
-  //}),
+  hideSideBar: Ember.computed('currentRouteName',function(){
+    var curPath = this.get('currentRouteName');
+    console.log(curPath);
+    if(curPath === 'index' || curPath === 'sign-up.installation' || curPath === 'sign-up.auth' || curPath === 'sign-up.choose-plan' || curPath === 'sign-up.goal' || curPath === 'sign-up.getting-started'){
+      return true;
+    }
+    return false;
+  }),
   hideNav: function() {
     var curPath =this.get('currentPath');
     if(curPath == 'sign-up'){
