0ffd145c1bf20efa1fb72cec960e5735c148c162
Rosie Yohannan <rosie.yohannan@reconfigure.io>
2018-08-16 09:44:53 +0100

    try tabs for getting started

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ sys.path.insert(0, os.path.abspath('.'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.todo', 'hidden_code_block', 'subst_code_block']
+extensions = ['sphinx.ext.todo', 'hidden_code_block', 'subst_code_block', 'sphinx_tabs.tabs']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
